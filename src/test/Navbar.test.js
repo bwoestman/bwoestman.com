815897da146cbb40c45054766230373e814c57e9
Brian Woestman <woestmab@gmail.com>
2018-08-05 16:55:47 -0500

    test(jest): Setup testing framework and write base tests.  resolves #5

--- src/test/Navbar.test.js
+++ src/test/Navbar.test.js
@@ -2,11 +2,31 @@ import React from "react";
 import ShallowRenderer from "react-test-renderer/shallow";
 
 import Navbar from "../navbar/Navbar";
+import NavModalButton from "../navbar/NavModalButton";
 
-it("has two NavModalButtons", () => {
-  const renderer = new ShallowRenderer();
-  renderer.render(<Navbar />);
-  const result = renderer.getRenderOutput();
+let renderer;
+let result;
+const title = "bdub";
 
-  expect(result.type).toBe("div");
+beforeEach(() => {
+  renderer = new ShallowRenderer();
+  renderer.render(<Navbar title={title} />);
+  result = renderer.getRenderOutput();
+});
+
+it("is a nav element", () => {
+  expect(result.type).toBe("nav");
+});
+
+it("has the correct title", () => {});
+
+it("contains three children", () => {
+  expect(result.props.children).toHaveLength(3);
+});
+
+it("contain Logo with title, Login button & Sign Up button", () => {
+  expect(result.props.children[0].type).toBe("a");
+  expect(result.props.children[0].props.children).toEqual(title);
+  expect(result.props.children[1].props.title).toEqual("Login");
+  expect(result.props.children[2].props.title).toEqual("Sign Up");
 });
