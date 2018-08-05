c699e2509079e7585090d970c472a2ca2fb97f46
Brian Woestman <woestmab@gmail.com>
2018-08-04 21:07:45 -0500

    fix(): Create unique id for passwordVerification input

---
+++ src/navbar/NavModalButton.jsx
@@ -0,0 +1,109 @@
+import * as React from "react";
+
+function NavModalButton(props)
+{
+    return (
+        <div>
+            <button
+                type="button"
+                className="btn btn-primary"
+                data-toggle="modal"
+                data-target={"#" + props.dataTarget}
+            >
+                {props.children}
+                <span className="align-middle">{props.title}</span>
+            </button>
+            <NavModal id={props.dataTarget} title={props.title} verify={props.verify}/>
+        </div>
+    );
+}
+
+function NavModal(props)
+{
+    let passwordVerification = "";
+    if (props.verify)
+    {
+        passwordVerification = (
+            <div className="form-group">
+                <label htmlFor={props.id + "passwordVerification"}>Password Verification</label>
+                <input
+                    type="password"
+                    className="form-control"
+                    id={props.id + "passwordVerification"}
+                    placeholder="Password Verification"
+                />
+            </div>
+        )
+    }
+
+    return (
+        <div
+            className="modal fade"
+            id={props.id}
+            tabIndex="-1"
+            role="dialog"
+            aria-labelledby={props.id + "Label"}
+            aria-hidden="true"
+        >
+            <div className="modal-dialog" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id={props.id + "Label"}>
+                            {props.title}
+                        </h5>
+                        <button
+                            type="button"
+                            className="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                        >
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <form className="form-group">
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <label htmlFor={props.id + "email"}>Email address</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id={props.id + "email"}
+                                    aria-describedby="emailHelp"
+                                    placeholder="Enter email"
+                                />
+                                <small id={props.id + "emailHelp"}
+                                       className="form-text text-muted">
+                                    Your email will never be shared with anyone else.
+                                </small>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor={props.id + "password"}>Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    id={props.id + "password"}
+                                    placeholder="Password"
+                                />
+                            </div>
+                            {passwordVerification}
+                        </div>
+                        <div className="modal-footer">
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-dismiss="modal"
+                            >
+                                Cancel
+                            </button>
+                            <button type="submit" className="btn btn-primary">
+                                {props.title}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default NavModalButton;
