dc4beac662aaa6b1bad4dedda1f67dc88c9ed345
Brian Woestman <woestmab@gmail.com>
2018-08-11 00:41:43 -0500

    tests(enzyme):  Add watchman dependency to allow more open files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import App from "./App";
 import { shallow } from "enzyme";
-import AnotherComponent from "./AnotherComponenet";
+import Navbar from "./navbar/Navbar";
 
 it("renders without crashing", () => {
   const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
